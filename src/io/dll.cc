c7be88be1671892baa5c5ef0ba54d8929c2e024f
Raymond <101374892+raymonable@users.noreply.github.com>
2025-03-28 02:57:08 -0400

    refactor: another refactor :sob:

--- src/io/dll.cc
+++ src/io/dll.cc
@@ -3,6 +3,7 @@
 //
 
 #include <lib.hh>
+#include <io/exports.hh>
 
 BOOL WINAPI DllMain(
     HINSTANCE hinstDLL,  // handle to DLL module
@@ -10,4 +11,13 @@ BOOL WINAPI DllMain(
     LPVOID lpvReserved )  // reserved
 {
     return true;
+}
+
+// Common exports (x86 and x64)
+
+uint16_t chuni_io_get_api_version(void) {
+    return 0x0102;
+};
+HRESULT chuni_io_jvs_init(void) {
+    return S_OK;
 }
--- src/io/exports.hh
+++ src/io/exports.hh
@@ -5,4 +5,11 @@
 #ifndef EXPORTS_HH
 #define EXPORTS_HH
 
+#include <lib.hh>
+
+extern "C" {
+    __declspec(dllexport) uint16_t chuni_io_get_api_version(void);
+    __declspec(dllexport) HRESULT chuni_io_jvs_init(void);
+};
+
 #endif //EXPORTS_HH
